2c7be3ef2b175e843181987a25277bb32dbdac37
eljorch75-tech <eljorch75@gmail.com>
2026-09-17 14:25:21 -0300

    conexion bd

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const pool = require("./bd/conexion")
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+async function probarConexion() {
+ try {
+    await pool.query ("SELECT 1");
+    console.log("conexion exitosa")
+ } catch (error) {
+     console.log("Error en la conexion")
+     console.log(error.message)
+ }   
+}
 console.log("antes");
 
 app.listen(PORT, async() => {
     console.log(`servidor ejecutándose en http://localhost:${PORT}`);
+    await probarConexion()
 });
--- rutas/estudiantes.js
+++ rutas/estudiantes.js
@@ -14,4 +14,28 @@ router.post("/", async (req, res) => {
         telefono_tutor,
         email_tutor
     } = req.body;
+
+    try {
+        const [resultado] = await pool.execute(
+            `INSERT INTO estudiantes
+            (nombre, apellido, dni, fecha_nacimiento, nombre_tutor, telefono_tutor, email_tutor, id_estudiante) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`,
+            [
+              nombre,
+              apellido,
+              dni,
+              fecha_nacimiento,
+              nombre_tutor,
+              telefono_tutor,
+              email_tutor
+            ]
+        )
+        return res.status(201).json({
+            mensaje: "estudiantes agregado correctamente"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: "No se pudo agregar los estudiantes",
+            detalle: error.mensaje
+        })
+    }
 });
